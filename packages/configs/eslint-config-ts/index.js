51cce641c18bba53d97b68f2ae85b198fb4b5cd3
Mist <mist.zzh@gmail.com>
2023-03-02 17:38:26 +0800

    refactor: lint rules & add custom rules infrastructure

--- packages/configs/eslint-config-ts/index.js
+++ packages/configs/eslint-config-ts/index.js
@@ -2,23 +2,18 @@ const rules = require('./rules')
 
 module.exports = {
   parser: '@typescript-eslint/parser',
-
-  plugins: ['@typescript-eslint'],
-
+  plugins: ['@typescript-eslint', 'custom'],
   root: true,
-
   env: {
     browser: true,
     node: true,
     es6: true,
   },
-
   globals: {
     require: true,
     Promise: true,
     process: true,
   },
-
   parserOptions: {
     ecmaVersion: '2018',
     sourceType: 'module',
@@ -27,6 +22,5 @@ module.exports = {
       legacyDecorators: true,
     },
   },
-
   rules,
 }
